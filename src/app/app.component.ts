f0df0196d1351836cfc00ea39cb09c1231bed542
Ariel Hillel <arielhillel2023@gmail.com>
2025-05-22 01:22:59 +0300

    done with the exsmple

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,38 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, effect, ElementRef, signal, Signal, viewChild } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'ng-collapse-signal';
+    expanded = signal(true);
+    items = signal(['פריט א', 'פריט ב', 'פריט ג']);
+
+    contentRows: Signal<ElementRef> = viewChild.required<ElementRef>('contentRows');
+
+    contentHeight = signal(0);
+
+    constructor() {
+        effect(() => {
+            this.items();
+            requestAnimationFrame(() => {
+                if (this.contentRows()) {
+                    this.contentHeight.set(this.contentRows().nativeElement.scrollHeight);
+                }
+            });
+        });
+    }
+
+    toggle() {
+        this.expanded.set(!this.expanded());
+    }
+
+    addItem() {
+        this.items.set([...this.items(), 'עוד פריט']);
+    }
+
+    removeItem() {
+        this.items.set(this.items().slice(0, -1));
+    }
 }
